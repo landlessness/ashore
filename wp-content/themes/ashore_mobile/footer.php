4e4f12be9ff1c65a234410577b851dc3943a9931
landlessness <brian@landlessness.net>
2014-07-04 03:52:48 -0400

    changed copyright to creative commons part 4

--- wp-content/themes/ashore_mobile/footer.php
+++ wp-content/themes/ashore_mobile/footer.php
@@ -37,12 +37,13 @@
         <div class="copyright">
         	<div class="container">
 
-            <div>This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.</div>
-            <div class="clear"></div>	
             <div>
               <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">
                 <img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" />
               </a>
+            </div>
+            <div class="creative-commons-text">
+              This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.
             </div>
           </div>
         </div>
