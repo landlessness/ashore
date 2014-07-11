a814ae598b6514025075cca6b4498a8225ac23d1
landlessness <brian@landlessness.net>
2014-07-11 14:01:01 -0400

    toggle for showing featured image in post part 8

--- wp-content/themes/ashore_mobile/single.php
+++ wp-content/themes/ashore_mobile/single.php
@@ -15,7 +15,7 @@ echo $category->slug . ' ';} ?>"></span><?php the_category(' '); ?> </div>
                     <?php } ?>
                 </div>
             </div>   
-            <?php $hide_featured_image = the_field('hide_featured_image') ?>
+            <?php $hide_featured_image = get_field('hide_featured_image') ?>
             <div>$hide_featured_image: <?php $hide_featured_image ?></div>
             <?php if ( has_post_thumbnail() && $hide_featured_image <> "1") { ?>
                     <div class="featured-thumbnail"><?php the_post_thumbnail(); ?></div> 
