c5a40700edf92a13c586e71dadb9857805b84117
landlessness <brian@landlessness.net>
2014-07-15 17:55:04 -0400

    custom feed for mail chimp part 2

--- wp-content/themes/ashore_mobile/page-mail-chimp-feed.php
+++ wp-content/themes/ashore_mobile/page-mail-chimp-feed.php
@@ -23,19 +23,22 @@ $lastpost = $numposts - 1;
 
 header("Content-Type: application/rss+xml; charset=UTF-8");
 echo '<?xml version="1.0"?>';
-?><rss version="2.0">
+?><rss version="2.0" xmlns:media="http://search.yahoo.com/mrss">
 <channel>
-  <title>Yoast E-mail Update</title>
-  <link>http://yoast.com/</link>
-  <description>The latest blog posts from Yoast.com.</description>
+  <title>Ashore Detroit Newsletter</title>
+  <link>http://detroit.ashore.me/</link>
+  <description>Detroit's weekly water sports and lifestyle newsletter.</description>
   <language>en-us</language>
   <pubDate><?php yoast_rss_date( strtotime($ps[$lastpost]->post_date_gmt) ); ?></pubDate>
   <lastBuildDate><?php yoast_rss_date( strtotime($ps[$lastpost]->post_date_gmt) ); ?></lastBuildDate>
-  <managingEditor>joost@yoast.com</managingEditor>
+  <managingEditor>detroit@ashore.me</managingEditor>
 <?php foreach ($posts as $post) { ?>
   <item>
     <title><?php echo get_the_title($post->ID); ?></title>
     <link><?php echo get_permalink($post->ID); ?></link>
+    <?php if(get_the_post_thumbnail()): ?>
+        <media:content url="<?php echo wp_get_attachment_url(get_post_thumbnail_id($post->ID)); ?>" medium="image" />
+    <?php endif; ?>
     <description><?php echo '<![CDATA['.yoast_rss_text_limit($post->post_content, 500).'<br/><br/>Keep on reading: <a href="'.get_permalink($post->ID).'">'.get_the_title($post->ID).'</a>'.']]>';  ?></description>
     <pubDate><?php yoast_rss_date( strtotime($post->post_date_gmt) ); ?></pubDate>
     <guid><?php echo get_permalink($post->ID); ?></guid>
