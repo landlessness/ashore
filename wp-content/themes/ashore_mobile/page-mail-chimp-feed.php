8c785450125cdde39e907accd7942db443cc3b31
landlessness <brian@landlessness.net>
2014-07-15 20:37:16 -0400

    custom feed for mail chimp part 20

--- wp-content/themes/ashore_mobile/page-mail-chimp-feed.php
+++ wp-content/themes/ashore_mobile/page-mail-chimp-feed.php
@@ -22,7 +22,6 @@ $posts = query_posts('showposts='.$numposts);
 $lastpost = $numposts - 1;
 
 header("Content-Type: application/rss+xml; charset=UTF-8");
-echo '<?xml version="1.0"?>';
 ?><rss version="2.0" xmlns:media="http://search.yahoo.com/mrss">
 <channel>
   <title>Ashore Detroit Newsletter</title>
